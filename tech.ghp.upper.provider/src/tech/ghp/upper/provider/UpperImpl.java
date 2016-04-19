bd3a38c28535447235dacd311423fc0c8aedc921
ghpopovici <ghpopovici@gmail.com>
2016-04-19 02:20:40 +0200

    minor change

--- tech.ghp.upper.provider/src/tech/ghp/upper/provider/UpperImpl.java
+++ tech.ghp.upper.provider/src/tech/ghp/upper/provider/UpperImpl.java
@@ -10,6 +10,8 @@ import tech.ghp.upper.api.Upper;
 @Component(name = "tech.ghp.upper")
 public class UpperImpl implements Upper{
 	public String upper(String input){
+		
 		return input.toUpperCase();
+		
 	}
 }
